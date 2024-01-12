7e079fcb51fe6b8c6a32d03889006817a269f28e
Vladyslava Sysenko <vlada.sysenko.2002@gmail.com>
2024-01-12 09:14:59 +0200

    Edit HT-18

--- HT-18/searsapp/products/services.py
+++ HT-18/searsapp/products/services.py
@@ -8,37 +8,41 @@ from .scraper import ProductScraper
 
 
 def save_scraped_products(product_ids: list[str]) -> None:
+    command = (
+        'python manage.py shell --command="from products.services import'
+        f" _save_products_details; _save_products_details({product_ids})"
+    )
+    subprocess.run(
+        command, shell=True, check=True, stdin=stdin, stdout=stdout, stderr=stderr
+    )
+
+
+def _save_products_details(product_ids: list[str]) -> None:
     for product_id in product_ids:
-        command = (
-            'python manage.py shell --command="from products.services import'
-            f" _save_product_details; _save_product_details('{product_id}')"
-        )
-        subprocess.run(command, shell=True, check=True, stdin=stdin, stdout=stdout, stderr=stderr)
+        scraping_result = ProductScraper(product_id).get_product_details()
+        if scraping_result.success:
+            product = scraping_result.product_info
 
+            # Save product to db
+            Product.objects.create(
+                site_id=product.id,
+                name=product.name,
+                link=product.link,
+                brand=product.brand,
+                category=product.category,
+                store=product.store,
+                current_price=product.current_price,
+                old_price=product.old_price,
+                description=product.description,
+            )
+            print(f"Product {product_id} is successfully added.")
+        else:
+            print(
+                f"Product {product_id} is not added. Error: {scraping_result.message}"
+            )
 
-def _save_product_details(product_id: str) -> None:
-    scraping_result = ProductScraper(product_id).get_product_details()
-    if scraping_result.success:
-        product = scraping_result.product_info
-
-        # Save product to db
-        Product.objects.create(
-            site_id=product.id,
-            name=product.name,
-            link=product.link,
-            brand=product.brand,
-            category=product.category,
-            store=product.store,
-            current_price=product.current_price,
-            old_price=product.old_price,
-            description=product.description,
-        )
-        print(f"Product {product_id} is successfully added.")
-    else:
-        print(f"Product {product_id} is not added. Error: {scraping_result.message}")
-
-    # Give time for site so that it won't block other requests
-    time.sleep(0.5)
+        # Give time for site so that it won't block other requests
+        time.sleep(0.5)
 
 
 def is_valid_ids(ids: list[str]) -> bool:
